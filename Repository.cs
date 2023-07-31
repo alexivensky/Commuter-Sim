c2eb00457fc7249ccca43fa46c014f857a64ba92
alexivensky <awi4@pitt.edu>
2023-07-31 15:05:28 -0400

    removed unneeded PropertyChanged functionality

--- Repository.cs
+++ Repository.cs
@@ -29,29 +29,6 @@ namespace Commuter_Sim
             return Task.CompletedTask;
         }
 
-        public void TrainPropertyChange(Train train)
-        {
-            train.PropertyChanged += TrainPropertyChanged;
-        }
-
-        private void TrainPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            if (sender is Train train)
-            {
-                switch (e.PropertyName)
-                {
-                    case "Position":
-                        //send event to HotChocolate subscription
-                        _sender.SendAsync(nameof(Subscription.TrainAdded), train);
-                        break;
-                    case "velocity":
-                        break;
-                    case "Acceleration":
-                        break;
-                }
-                    
-            }
-            
-        }
+        
     }
 }
