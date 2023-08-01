ba3d11e6671171e62e6c6a8551e83865ba872dcf
NEXT\201051966 <alex.ivensky-ext@alstomgroup.com>
2023-08-01 13:17:20 -0400

    minor change. added a comment

--- Mutation.cs
+++ Mutation.cs
@@ -7,6 +7,12 @@ using HotChocolate.Subscriptions;
 
 namespace Commuter_Sim
 {
+    /**
+     * Client-Side Mutations.
+     * Can be used through the HotChocolate IDE to add, remove, or modify objects.
+     * Cannot be used on the server side.
+     */
+
     public class Mutation
     {
         public async Task<TrainPayload> AddTrain(TrainInput input, [Service] Repository repository, [Service] ITopicEventSender sender)
@@ -18,7 +24,7 @@ namespace Commuter_Sim
         }
 
 
-        public record TrainPayload(Train record);
+        public record TrainPayload(Train train);
         public record TrainInput(double pos, double vel, double acc);
     }
 }
