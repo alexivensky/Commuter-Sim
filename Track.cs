5e1991f7d8a975f16a6c2e55fa2ad23d4ee4578e
NEXT\201051966 <alex.ivensky-ext@alstomgroup.com>
2023-08-07 13:17:39 -0400

    added train track class

---
+++ Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commuter_Sim
+{
+    public class Track
+    {
+        private List<Train> _trainsOnTrack = new List<Train>();
+        private int _trackLength;
+
+        private Station _stationA, _stationB;
+
+        public Track(int trackLength, Station stationA, Station stationB)
+        {
+            _trackLength = trackLength;
+            _stationA = stationA;
+            _stationB = stationB;
+        }
+    }
+}
