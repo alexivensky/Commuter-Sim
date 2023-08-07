92ef9729e68e7483a9fb417676d6e5ec11154347
NEXT\201051966 <alex.ivensky-ext@alstomgroup.com>
2023-08-07 13:17:05 -0400

    added Station class

---
+++ Station.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commuter_Sim
+{
+    public class Station
+    {
+        private List<Train> _trainsAtStation = new();
+        private string _stationName;
+
+        public Station(string StationName)
+        {
+            _stationName = StationName;
+        }
+
+        public List<Train> TrainsAtStation
+        {
+            get => _trainsAtStation;
+        }
+
+        public string StationName
+        {
+            get => _stationName;
+            set => _stationName = value;
+        }
+    }
+}
